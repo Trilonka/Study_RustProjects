0ee20c0337a5c30f464422b3d4fa99b14306bed7
Trilonka <aniskov.stepan@mail.ru>
2022-05-28 18:02:49 +0300

    add functions!

---
+++ functions/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+
+    print_labeled_measurement(5, 'h');
+
+    let y = { // Выражение - Заканачивается оператором
+        let x = 3; // Оператор
+        x +1 // Выражение 
+    };
+    println!("The value of y is: {y}");
+
+    let x = five();
+    println!("The value of x is: {x}");
+
+    let x = plus_one(x); // Затенение
+    println!("The value of x is: {x}"); 
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5 // Выражение 
+}
+
+fn plus_one(x: i32) -> i32 {
+    x+1
+}
