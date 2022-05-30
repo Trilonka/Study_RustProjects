0ed2271e675fb3b67abf7ab98013bb5c12614ba7
Trilonka <aniskov.stepan@mail.ru>
2022-05-30 12:39:48 +0300

    add structures

---
+++ structures/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1 // все другие поля будут скопированы в user2 from user1
+    }; // now we can't use user1, because username(String) user1 move into username(String) user2.
+    // if we would change username too, we would can use user1 
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
