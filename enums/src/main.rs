5a968a86c12a9c1c39ee02953e75a06f81fee6f5
Trilonka <aniskov.stepan@mail.ru>
2022-05-31 23:07:39 +0300

    some changes

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,11 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u32, u32, u32, u32),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v4 = IpAddr::V4(127, 0, 0, 1);
+    let v6 = IpAddr::V6(String::from("::1"));
+    println!("{:#?}, {:#?}", v4, v6);
 }
