24e6e222694eaa6730dce6a830b31ce0fc99a19c
Trilonka <aniskov.stepan@mail.ru>
2022-06-01 22:31:50 +0300

    some changes

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-enum IpAddr {
-    V4(String),
-=======
 #[derive(Debug)]
 enum IpAddr {
     V4(u32, u32, u32, u32),
->>>>>>> 5a968a86c12a9c1c39ee02953e75a06f81fee6f5
     V6(String),
 }
 
 fn main() {
-<<<<<<< HEAD
-    let four = IpAddr::V4(String::from("127.0.0.1"));
-    let six = IpAddr::V6(String::from("::1"));
-=======
     let v4 = IpAddr::V4(127, 0, 0, 1);
     let v6 = IpAddr::V6(String::from("::1"));
     println!("{:#?}, {:#?}", v4, v6);
->>>>>>> 5a968a86c12a9c1c39ee02953e75a06f81fee6f5
 }
