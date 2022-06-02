914b16cbcfb1e50c93c5121a482eb519971c1a03
Trilonka <aniskov.stepan@mail.ru>
2022-06-02 22:55:51 +0300

    some...

---
+++ chapter_8_tasks/src/main.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut a = vec![1,2,3,4,5,6,12,12,12,85,42,7,9,9,9];
+    println!("{:#?}", first(&mut a))
+}
+
+fn first(v: &mut Vec<i32>) -> (f32, i32, i32) {
+    let average = v.iter().sum::<i32>() as f32 / v.len() as f32;
+
+    v.sort();
+    let mediane = v[v.len()/2];
+
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for &mut el in v {
+        *map.entry(el).or_insert(0) += 1;
+    }
+    let mode = map
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(val, _)| val)
+        .expect("Cannot compute ht mode of zero number");
+
+    (average, mediane, mode)
+}
--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+use std::iter::Iterator;
+
 fn main() {
-    println!("Hello, world!");
+    let mut scores = HashMap::new();
+    let cor = [(String::from("Blue"), 10),
+                                       (String::from("Yellow"), 50)];
+    
+    let a: HashMap<_, _> = cor.into_iter().collect();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    for (key, value) in &scores { // Печатает в произвольном порядке
+        println!("{}: {}", key, value);
+    }
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("blue"), 10);
+    scores.entry(String::from("yellow")).or_insert(50);
+    scores.entry(String::from("blue")).or_insert(50);
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
