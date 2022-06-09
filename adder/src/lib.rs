392a23a78f2ccab9ba9b40f9b473ca51c0e29c44
Trilonka <aniskov.stepan@mail.ru>
2022-06-09 22:22:44 +0300

    add adder

---
+++ adder/src/lib.rs
@@ -0,0 +1,87 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Fail this test"); // failed test
+    // }
+
+    #[test]
+    fn larget_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2))
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Karol");
+        assert!(
+            result.contains("Karol"),
+            "Greeting didn't contains name, result is '{}'", result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Значение догадки должно быть меньше или равно 100")] // true when panic
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
+fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+fn greeting(name: &str) -> String {
+    format!("Greeting, {}!", name)
+}
+
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Значение догадки должно быть больше или равно 1, 
+            получено {}.", value);
+        } else if value > 100 {
+            panic!("Значение догадки должно быть меньше или равно 100, 
+            получено {}.", value);
+        }
+        Guess { value }
+    }
+}
